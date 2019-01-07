6df017903c5c972082ba8b6a908d8477dae8cf55
Resty Alejo <restyjayalejo17@gmail.com>
2019-01-07 23:01:46 +0800

    new update

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,3 +83,10 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+
+if($_SERVER['SERVER_NAME'] == 'localhost') {
+	define('_BASE_URL_', 'http://localhost/finquor/');
+} else {
+	define('_BASE_URL_', 'http://finquor.com/');
+}

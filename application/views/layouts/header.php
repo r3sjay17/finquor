ee039889faf620e54b438cac850b362c20f0cb44
Resty Alejo <restyjayalejo17@gmail.com>
2019-02-09 00:59:36 +0800

    new update

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -7,6 +7,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
 	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+	<meta name="google-site-verification" content="hmVjWeDuTLSw69wWLjm4qxvCvwbQjz9bA78CLiOIYzU" />
 
 	<title><?=$page?> - FinQuor</title>
 
